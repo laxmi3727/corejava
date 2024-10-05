891d2ff652a0623fba392ca88e2aabf8caf6d0d9
laxmi <laxmi@laxmi>
2024-10-05 12:17:44 +0530

    demo

---
+++ variables/src/com/taetae/variables/Variables.java
@@ -0,0 +1,24 @@
+package com.taetae.variables;
+
+public class Variables {
+       
+	//valid identifiers
+	   int num1;
+	   int _num2;   
+	   int $num3;
+	   
+	//invalid identifiers
+	   
+	  // int class;
+	 //  int 123a; 
+	  // int &num1;  
+	         
+	public static void main(String[] args) {
+		 
+		System.out.println("int num1");
+		
+		
+		
+	}
+	
+}
